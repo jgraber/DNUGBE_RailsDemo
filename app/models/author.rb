cdfe05ead0d924cc68579dcf5e342688261913f0
Johnny Graber <dev@jgraber.ch>
2012-10-29 23:07:47 +0100

    add n:m relationship

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ActiveRecord::Base
   attr_accessible :firstname, :lastname, :twitter
+
+  has_many :authorships
+  has_many :books, :through => :authorships
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ActiveRecord::Base
-  attr_accessible :description, :isbn, :title
+  attr_accessible :description, :isbn, :title, :authors_attributes
+
+  has_many :authorships
+  has_many :authors, :through => :authorships
+  accepts_nested_attributes_for :authors, :reject_if => lambda { |a| a[:lastname].blank? }, :allow_destroy => true
 end
