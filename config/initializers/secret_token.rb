cb9178d85ec094cc97ca3739181828792d819d6d
Johnny Graber <dev@jgraber.ch>
2012-10-29 22:02:33 +0100

    start with plain new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dnugbooks::Application.config.secret_token = 'b5ce78d063657a932865514b1493a2ae73dad8fe9d0af5bc682c4b0f5bf0d885e4373ba8ed53c5758bf4a443cb4ab9a330450804cda056a655898213fcd7d91c'
